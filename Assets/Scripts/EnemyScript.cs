021a0176cc84917dfc29dcea2081c6fd65ee92a8
Unknown <citrusspoon@gmail.com>
2017-12-13 22:04:48 -0500

    Enemy successfully moves along path

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	public float speed = 10f;
+	//reference to this object's Transform component
+	private Transform thisTransform;
+
+	private Transform target;
+	private int nextWaypointIndex = 0;
+
+	void Awake(){
+		thisTransform = transform;
+	}
+
+
+	void Start(){
+		target = WaypointScript.waypoints[0];
+	}
+
+	//direction the enemy is moving
+	Vector3 dir;
+
+	void Update(){
+		dir = target.position - thisTransform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+
+
+
+		if (Vector3.Distance (thisTransform.position, target.position) < 0.4f)
+			GetNextWaypoint ();
+	}
+
+	void GetNextWaypoint(){
+
+		if (nextWaypointIndex >= WaypointScript.waypoints.Length - 1)
+			this.gameObject.SetActive (false);
+		else {
+			nextWaypointIndex++;
+			target = WaypointScript.waypoints [nextWaypointIndex];
+		}
+	}
+}
---
+++ Assets/Scripts/WaypointScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointScript : MonoBehaviour {
+
+	public static Transform[] waypoints;
+
+	void Awake(){
+		waypoints = new Transform[transform.childCount];
+		for (int i = 0; i < waypoints.Length; i++) {
+			waypoints [i] = transform.GetChild (i);
+		}
+
+		print (waypoints[0].transform.localPosition.x);
+	}
+
+}
