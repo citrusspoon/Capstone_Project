eec554b1eafddcbfed3a6218eeafe8f66fadb4b7
Unknown <citrusspoon@gmail.com>
2018-03-19 19:27:12 -0400

    Camera now smoothly transitions between views

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,30 +4,61 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	public bool gameView = true;
-	public Transform gameViewTransform;
-	public Transform quizletViewTransform;
+	public WindowState currentWindow;
+	public Transform gameWindowTransform;
+	public Transform importMenuTransform;
+	public Transform mainMenuTransform;
 	public GameObject testUI;
 	public GameObject quizletElements;
 	private Transform thisTransform;
+
+	private bool moving = false;
+	public float camSpeed = 100f;
 	// Use this for initialization
 	void Start () {
-
+		thisTransform = transform;
+		MoveTo (WindowState.Game);
 	}
-	
+	Vector3 dir;
+	Transform target;
 	// Update is called once per frame
 	void Update () {
+
+		if (moving) { 
+			switch (currentWindow) {
+			case WindowState.Game:
+				target = gameWindowTransform;
+				break;
+			case WindowState.ImportMenu:
+				target = importMenuTransform;
+				break;
+			case WindowState.MainMenu:
+				target = mainMenuTransform;
+				break;
+			}
+
+
+			dir = target.position - thisTransform.position;
+			transform.Translate (dir.normalized * camSpeed * Time.deltaTime, Space.World);
+
+			if (Vector3.Distance (thisTransform.position, target.position) < 2f) {
+				moving = false;
+			}
+		}
 		
+	}
+	public void MoveTo(WindowState s){
+		currentWindow = s;
+		moving = true;
 	}
 
 	public void SwapCamera(){
-		if (gameView)
-			transform.position = quizletViewTransform.position;
+		if (currentWindow == WindowState.Game)
+			MoveTo(WindowState.ImportMenu);
 		else
-			transform.position = gameViewTransform.position;
+			MoveTo(WindowState.Game);
 
-		gameView = !gameView;
-		testUI.SetActive (gameView);
-		quizletElements.SetActive (!gameView);
+		testUI.SetActive (currentWindow == WindowState.Game);
+		quizletElements.SetActive (currentWindow == WindowState.ImportMenu);
 	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public enum TargetingMode {First, Last, Closest, Furthest};
 public enum TurretType {Basic, Slow, Rocket, FRPowerup, RangePowerup, Destroy, Guard};
 public enum GameState {WaveActive, WaveInactive};
+public enum WindowState {Game, ImportMenu, MainMenu};
 
 public class GameController : MonoBehaviour {
 
@@ -15,6 +16,9 @@ public class GameController : MonoBehaviour {
 	public TouchManager touchManagerRef;
 	public TurretManager turretManagerRef;
 	public TurretTray turretTrayRef;
+
+	private bool toggling = false;
+	public float traySpeed = 10f;
 
 
 	void Awake()
